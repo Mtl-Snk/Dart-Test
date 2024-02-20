326f6b9c388995a26db1ea34f585f3e79adced05
mtl-snk <estradaf.carlos@gmail.com>
2024-02-20 17:19:51 -0600

    primer commit

---
+++ helloworld.dart
@@ -0,0 +1,27 @@
+void main() {
+  print("Hola, Dart!!");
+
+var myString = "Esto es una cadena de texto";
+myString = "aqui cambio el valor de la cadena de texto";
+//helloDart = 6; || Error
+print (myString);
+
+String myString2 = "Hola Dart 2 !";
+print (myString2);
+
+var myInt = 6;
+myInt = myInt + 4;
+print(myInt);
+print(myInt - 1);
+print(myInt);
+
+var myDouble = 6.5;
+print(myDouble);
+myDouble = 6;
+print(myDouble);
+
+var myBool = false;
+myBool = true;
+print(myBool);
+
+}
